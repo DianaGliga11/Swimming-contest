41d23664611becec5321a49abc0e35f94e870f51
DianaGliga11 <diana.adelina.gliga@stud.ubbcluj.ro>
2025-03-18 17:38:59 +0200

    Am realizat doua module: 1. Module - pentru clasele utiolizate ulterior in Swiming Competition - contine o clasa Entity abstracta care va da ID-ul fiecarei clase derivate 2. Repository - pentru a gestiona obiectele din Module - o interfata generala, pentru fiecare intervata ulterioara cu metodele generale - interfete pentru fiecare obiect, pentru a introduce noi metode ce vor fi ulterior implementate Tot proiectul este legat folosind gradle: in settings.gradle se introduc modulele create pentru a putea naviga intre ele obiecte/interfete etc. Ambele module pot fi rulate, insa nu acest lucru se doreste,  urmand sa introduc un nou modul care imi permite rularea intregului proiect.
    Nou adaugate:
    1. Implementarea Repository-lor care utilizeaza baze de date SQLite si fisiere de configurare 'db.config'.
    2. Partea de jurnalizare folosind un fisier de tip xml. Tot ce reiese din logger va fi salvat in fisierul app.log.
    3. Mici modificari la nivel de Model (Entity) pentru a folosi incrementarea automata a ID-ului si a pastra unicitatatea acestuia in tabelele baze de date (primary key).

--- Repository/src/main/java/org/example/EventDBRepository.java
+++ Repository/src/main/java/org/example/EventDBRepository.java
@@ -9,25 +9,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-public class EventDBRepository implements EventRepository{
+public class EventDBRepository implements EventRepository {
+
     private JdbcUtils dbUtils;
     private static final Logger logger = LogManager.getLogger();
 
-    public EventDBRepository(Properties props){
+    public EventDBRepository(Properties props) {
         logger.info("Initializing EventDBRepository with properties: {} ", props);
-        dbUtils=new JdbcUtils(props);
+        dbUtils = new JdbcUtils(props);
     }
+
     @Override
     public void add(Event entity) throws EntityRepoException {
         logger.traceEntry("add task {} ", entity);
         Connection connection = dbUtils.getConnection();
         String sql = "INSERT INTO Events(style, distance) values (?,?)";
-        try(PreparedStatement ps = connection.prepareStatement(sql)){
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, entity.getStyle());
             ps.setInt(2, entity.getDistance());
             ps.executeUpdate();
             logger.traceExit("task {} added", entity);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             logger.error(e);
             System.err.println("Error DB " + e);
             throw new EntityRepoException(e);
@@ -36,15 +38,15 @@ public class EventDBRepository implements EventRepository{
     }
 
     @Override
-    public void remove(Event entity) throws EntityRepoException{
+    public void remove(Event entity) throws EntityRepoException {
         logger.traceEntry("remove task {} ", entity);
         Connection connection = dbUtils.getConnection();
         String sql = "DELETE FROM Events WHERE id=?";
-        try(PreparedStatement ps = connection.prepareStatement(sql)){
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, entity.getId());
             ps.executeUpdate();
             logger.traceExit("task {} removed", entity);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             logger.error(e);
             System.err.println("Error DB " + e);
             throw new EntityRepoException(e);
@@ -58,9 +60,9 @@ public class EventDBRepository implements EventRepository{
         Connection connection = dbUtils.getConnection();
         List<Event> events = new ArrayList<>();
         String sql = "SELECT * FROM Events";
-        try(PreparedStatement ps = connection.prepareStatement(sql)){
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next()){
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
                     int id = rs.getInt("id");
                     String style = rs.getString("style");
                     int distance = rs.getInt("distance");
@@ -70,7 +72,7 @@ public class EventDBRepository implements EventRepository{
                     logger.traceExit("task {} added", event);
                 }
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             logger.error(e);
             System.err.println("Error DB " + e);
             throw new EntityRepoException(e);
@@ -83,10 +85,10 @@ public class EventDBRepository implements EventRepository{
         logger.traceEntry("task findById {}", id);
         Connection connection = dbUtils.getConnection();
         String sql = "SELECT * FROM Events WHERE id=?";
-        try(PreparedStatement ps = connection.prepareStatement(sql)){
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, id);
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next()){
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
                     String style = rs.getString("style");
                     int distance = rs.getInt("distance");
                     Event event = new Event(style, distance);
@@ -95,7 +97,7 @@ public class EventDBRepository implements EventRepository{
                     return event;
                 }
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             logger.error(e);
             System.err.println("Error DB " + e);
             throw new EntityRepoException(e);
@@ -108,13 +110,13 @@ public class EventDBRepository implements EventRepository{
         logger.traceEntry("update task {} ", entity);
         Connection connection = dbUtils.getConnection();
         String sql = "UPdate Events SET style=?, distance=? WHERE id=?";
-        try(PreparedStatement ps = connection.prepareStatement(sql)){
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, entity.getStyle());
             ps.setInt(2, entity.getDistance());
             ps.setInt(3, entity.getId());
             ps.executeUpdate();
             logger.traceExit("task {} updated", entity);
-        }catch (SQLException e){
+        } catch (SQLException e) {
             logger.error(e);
             System.err.println("Error DB " + e);
             throw new EntityRepoException(e);
