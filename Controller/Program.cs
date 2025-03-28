9147132f98907640a72da033867a21a0f860ad89
DianaGliga11 <diana.adelina.gliga@stud.ubbcluj.ro>
2025-03-28 12:14:33 +0200

    Repara Confirm pt NewEntry

---
+++ Controller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using SwimmingCompetitionController;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        IDictionary<string, string> properties = new Dictionary<string, string>
+        {
+            { "ConnectionString", "Data Source=SwimingContest.db" }
+        };
+        
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainController(properties));
+    }
+}
